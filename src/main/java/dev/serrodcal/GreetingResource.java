dae0644895fc86b37b10f3f5a966cbbb2f92786a
Sergio Rodríguez Calvo <SERGIO.RODRIGUEZ.CALVO@ing.com>
2024-09-06 12:45:09 +0200

    Hello with name endpoint

--- src/main/java/dev/serrodcal/GreetingResource.java
+++ src/main/java/dev/serrodcal/GreetingResource.java
@@ -2,6 +2,7 @@ package dev.serrodcal;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
@@ -12,6 +13,13 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from Quarkus REST";
+    }
+
+    @GET
+    @Path("/{name}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloWithName(@PathParam("name") String name) {
+        return "Hi, " + name;
     }
 
 }
